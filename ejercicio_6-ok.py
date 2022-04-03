639e1a35bd50f3441b8ba921960c629ec562f9a3
Ezequiel-Daniel <100642895+Ezequiel-Daniel@users.noreply.github.com>
2022-04-03 14:58:27 -0300

    Add files via upload
    Buenas tardes, les comparto algunos ejercicios que estuve realizando,me quedan algunos que los voy a ir realizando los proximos dias.
    saludos.

---
+++ ejercicio_6-ok.py
@@ -0,0 +1,43 @@
+# Bucles [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicio de secuencias numéricas
+
+# Pedir por consola dos números que representen el principio y fin de una
+# secuencia numérica.
+# Realizar un bucle "for" que recorra esa secuencia armada con "range"
+# y cuante cuantes números son negativos y cuantos números son mayor o igual a cero
+# Tener en cuenta que "range" no incluye el número de "fin" en su secuencia,
+# sino que va hasta el anterior
+
+from re import I
+
+
+inicio = int(input('Ingrese el primer número de la secuencia\n'))
+fin = int(input('Ingrese el último número de la secuencia\n'))
+
+cantidad_numeros_positivos = 0  # Inicializo el contador en 0
+cantidad_numeros_negativos =0
+
+# for ... in range(....)
+
+# Imprimir el valor de la cantidad de números positivos y negativos
+
+
+ 
+for i in range (inicio, fin +1):
+    if i >= 0:
+        cantidad_numeros_positivos +=1
+    else:
+        cantidad_numeros_negativos += 1      
+    
+print("la cantidad de numeros positivos son",cantidad_numeros_positivos)
+print("la cantidad de numeros negativos son",cantidad_numeros_negativos)    
+
+print("terminamos!")
